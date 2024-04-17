6db6b0e1cb899349df14b6985556883af9d92f3d
Jorshua <estradajorshua16@gmail.com>
2024-04-17 16:22:14 -0600

    changes

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:healty_life/widgets/widgets.dart';
 import 'package:healty_life/routes/app_routes.dart';
 import 'package:healty_life/screens/screens.dart';
 
@@ -9,18 +9,35 @@ class ArticleScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*appBar: AppBar(
-        title: const Text('Articles'),
-      ),*/
-      body: ListView(padding: EdgeInsets.zero, children: [
-        Container(
-          height: MediaQuery.of(context).size.height * 0.45,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                  image: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2024/01/22/20/10/bark-8526227_1280.jpg'),
-                  fit: BoxFit.cover)),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ListView(padding: const EdgeInsets.all(20), children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Articles',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineMedium
+                  ?.copyWith(color: Colors.black, fontWeight: FontWeight.w900),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              'Read the most interesting articles',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const SizedBox(height: 20),
+          ],
         )
       ]),
     );
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:healty_life/widgets/image_container.dart';

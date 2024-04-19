df9b2c601ef9be67116b0e23c5e4e622b26a5168
Jorshua <estradajorshua16@gmail.com>
2024-04-19 16:44:11 -0600

    finish screen articles

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -6,6 +6,7 @@ class Article extends Equatable {
   final String subtitle;
   final String body;
   final String author;
+  final String authorImageUrl;
   final String category;
   final String imageUrl;
   final DateTime createdAt;
@@ -16,6 +17,7 @@ class Article extends Equatable {
     required this.subtitle,
     required this.body,
     required this.author,
+    required this.authorImageUrl,
     required this.category,
     required this.imageUrl,
     required this.createdAt,
@@ -30,10 +32,12 @@ class Article extends Equatable {
       body:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ex turpis. Duis odio nibh, bibendum ut pharetra non, laoreet nec elit. Vestibulum non neque ante. Sed non magna erat. Pellentesque quis ex porta, pretium tortor eu, eleifend lectus. Curabitur ultricies tristique maximus. Morbi lobortis vehicula dignissim. Proin cursus, tortor in malesuada sagittis, lorem ipsum porta risus, at sodales mi quam ut nisi. Aenean metus libero, venenatis vitae nibh vitae, porttitor iaculis purus. In est libero, tempus tempus purus sit amet, consequat faucibus ligula. Aliquam quis leo lacinia, euismod nibh sit amet, lacinia magna. Mauris laoreet nulla in libero tincidunt mollis.',
       author: 'Author 1',
+      authorImageUrl:
+          'https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947_1280.jpg',
       category: 'Salud',
       imageUrl:
           'https://cdn.pixabay.com/photo/2023/08/02/17/53/peach-8165738_1280.jpg',
-      createdAt: DateTime.now().subtract(const Duration(hours: 5)),
+      createdAt: DateTime.now().subtract(const Duration(hours: 1)),
     ),
     Article(
       id: '2',
@@ -43,10 +47,12 @@ class Article extends Equatable {
       body:
           '2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ex turpis. Duis odio nibh, bibendum ut pharetra non, laoreet nec elit. Vestibulum non neque ante. Sed non magna erat. Pellentesque quis ex porta, pretium tortor eu, eleifend lectus. Curabitur ultricies tristique maximus. Morbi lobortis vehicula dignissim. Proin cursus, tortor in malesuada sagittis, lorem ipsum porta risus, at sodales mi quam ut nisi. Aenean metus libero, venenatis vitae nibh vitae, porttitor iaculis purus. In est libero, tempus tempus purus sit amet, consequat faucibus ligula. Aliquam quis leo lacinia, euismod nibh sit amet, lacinia magna. Mauris laoreet nulla in libero tincidunt mollis.',
       author: 'Author 2',
+      authorImageUrl:
+          'https://cdn.pixabay.com/photo/2017/06/24/02/56/art-2436545_1280.jpg',
       category: 'Dietas',
       imageUrl:
           'https://cdn.pixabay.com/photo/2018/03/13/18/30/food-3223286_1280.jpg',
-      createdAt: DateTime.now().subtract(const Duration(hours: 5)),
+      createdAt: DateTime.now().subtract(const Duration(hours: 7)),
     ),
     Article(
       id: '3',
@@ -56,10 +62,12 @@ class Article extends Equatable {
       body:
           '3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut ex turpis. Duis odio nibh, bibendum ut pharetra non, laoreet nec elit. Vestibulum non neque ante. Sed non magna erat. Pellentesque quis ex porta, pretium tortor eu, eleifend lectus. Curabitur ultricies tristique maximus. Morbi lobortis vehicula dignissim. Proin cursus, tortor in malesuada sagittis, lorem ipsum porta risus, at sodales mi quam ut nisi. Aenean metus libero, venenatis vitae nibh vitae, porttitor iaculis purus. In est libero, tempus tempus purus sit amet, consequat faucibus ligula. Aliquam quis leo lacinia, euismod nibh sit amet, lacinia magna. Mauris laoreet nulla in libero tincidunt mollis.',
       author: 'Author 3',
+      authorImageUrl:
+          'https://cdn.pixabay.com/photo/2016/11/21/14/53/man-1845814_1280.jpg',
       category: 'Informacion',
       imageUrl:
           'https://cdn.pixabay.com/photo/2017/07/02/19/24/dumbbells-2465478_1280.jpg',
-      createdAt: DateTime.now().subtract(const Duration(hours: 5)),
+      createdAt: DateTime.now().subtract(const Duration(hours: 6)),
     ),
   ];
 
--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -24,9 +24,105 @@ class ArticleScreen extends StatelessWidget {
           children: [
             _ArticleHeadline(
               article: article,
-            )
+            ),
+            _ArticleBody(article: article),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _ArticleBody extends StatelessWidget {
+  const _ArticleBody({
+    super.key,
+    required this.article,
+  });
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CustomTag(
+                backgroundColor: Colors.black,
+                children: [
+                  CircleAvatar(
+                    radius: 10,
+                    backgroundImage: NetworkImage(article.authorImageUrl),
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    article.author,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 10.0),
+              CustomTag(
+                backgroundColor: Colors.grey.shade200,
+                children: [
+                  const Icon(
+                    Icons.access_time,
+                    color: Colors.grey,
+                    size: 15,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                      '${DateTime.now().difference(article.createdAt).inHours}h',
+                      style: Theme.of(context).textTheme.bodyMedium!),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            article.title,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  /* color: Colors.black,
+                      height: 1.25,*/
+                ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            article.body,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  height: 1.5,
+                ),
+          ),
+          const SizedBox(height: 10),
+          GridView.builder(
+              shrinkWrap: true,
+              itemCount: 2,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.25,
+              ),
+              itemBuilder: (contex, index) {
+                return ImageContainer(
+                    width: MediaQuery.of(context).size.width * 0.42,
+                    imageUrl:
+                        'https://cdn.pixabay.com/photo/2020/12/08/02/06/flowers-5813227_1280.jpg',
+                    margin: const EdgeInsets.only(right: 5.0, bottom: 5.0));
+              }),
+        ],
       ),
     );
   }
